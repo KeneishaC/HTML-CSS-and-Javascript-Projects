83e5fc56e69795972b36ad578b48570eba2ed728
keneishac <keneishacorbett@gmail.com>
2021-03-02 15:11:32 -0800

    adds funtions for active classes on images to show when clicked

---
+++ expanding-cards/script.js
@@ -0,0 +1,16 @@
+// alert('JS is loaded')
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
