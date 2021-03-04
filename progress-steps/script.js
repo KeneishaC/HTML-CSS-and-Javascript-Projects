0bd8b34b8efba6c9aeffede75245d9dbe82bf1ef
keneishac <keneishacorbett@gmail.com>
2021-03-03 18:45:29 -0800

    adds function for moving forward

---
+++ progress-steps/script.js
@@ -0,0 +1,16 @@
+// alert('JS is Loaded')
+
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const prev = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click',() => {
+    currentActive++
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+})
+
