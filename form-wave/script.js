98fedf2284b8db8ec4796f104ba0eb863294f4db
keneishac <keneishacorbett@gmail.com>
2021-03-08 14:25:26 -0800

    adds finishing touches

--- form-wave/script.js
+++ form-wave/script.js
@@ -5,6 +5,6 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 45}ms">${letter}</span>`)
+        .map((letter, idx) => `<span style="transition-delay:${idx * 30}ms">${letter}</span>`)
         .join('')
 })
