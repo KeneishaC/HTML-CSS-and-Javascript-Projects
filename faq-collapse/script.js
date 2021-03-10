9f28bd723ca681ece2fad5690b4b68cd9320da4a
keneishac <keneishacorbett@gmail.com>
2021-03-09 16:52:57 -0800

    adds function to open and close faq boxes

---
+++ faq-collapse/script.js
@@ -0,0 +1,9 @@
+// alert('Js is loaded')
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
