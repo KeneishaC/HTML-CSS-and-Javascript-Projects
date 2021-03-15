3fc0d9908279f089467bc4b5637ebea2c59680ce
keneishac <keneishacorbett@gmail.com>
2021-03-15 15:40:36 -0700

    adds functions for setting the background image and right button navigation

---
+++ background-slider/script.js
@@ -0,0 +1,37 @@
+// alert('JS is loaded')
+
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length -1){
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+//function that sets background image for active slide picture
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+//changes background image
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+
+    slides[activeSlide].classList.add('active')
+}
