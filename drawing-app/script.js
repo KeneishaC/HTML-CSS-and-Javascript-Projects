3d564bd46ff805cea62c6e7b6f476750f6f2aa58
keneishac <keneishacorbett@gmail.com>
2021-03-25 19:41:01 -0700

    adds functions that allow user drawing to show on canvas

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -5,9 +5,42 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20
+let isPressed = false
 let color = 'black'
 let x
 let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+    x = e.offsetX
+    y = e.offsetY
+
+    console.log(isPressed, x, y)
+
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+    x = undefined
+    y = undefined
+    
+    console.log(isPressed, x, y)
+    
+})
+canvas.addEventListener('mousemove', (e) => {
+  if(isPressed){
+
+      const x2 = e.offsetX
+      const y2 = e.offsetY
+
+      drawCircle(x2, y2)
+      drawLine(x, y, x2, y2)
+
+      x = x2
+      y = y2
+  }
+    
+})
 
 function drawCircle(x, y) {
     ctx.beginPath()
@@ -21,7 +54,8 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
+
 
