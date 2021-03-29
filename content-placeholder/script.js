60a64d15e92fda81f8983b5cd5f448b611ebe864
keneishac <keneishacorbett@gmail.com>
2021-03-29 16:53:55 -0700

    adds timeout that and removes the animation class to load content in the browser

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -5,8 +5,11 @@ const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
 
-const animated_bg = document.querySelectorAll('.animated-bg')
+const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+
+setTimeout(getData, 2500)
 
 
 function getData() {
@@ -16,4 +19,9 @@ function getData() {
     profile_img.innerHTML = ' <img src="https://randomuser.me/api/portraits/women/45.jpg" alt="">'
     name.innerHTML= 'Jane Doe'
     date.innerHTML = 'March 26, 2021'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
+
