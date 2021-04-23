86dc8041150460bc84276004511f88ca5367d7e0
keneishac <keneishacorbett@gmail.com>
2021-04-23 10:38:12 -0700

    adds fucntion to show user card when user is searched for

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -2,15 +2,38 @@ const APIURL = 'https://api.github.com/users/'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const main = document.getElementById('main')
 
 async function getUser(username) {
     try {
         const { data } =  await axios(APIURL + username)
-        console.log(data)
+        createUserCard(data)
         
     } catch (error) {
         console.log(error)
     }
+}
+
+function createUserCard(user){
+    const cardHTML = `
+    <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul>
+                    <li>${user.followers} <strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+                <div id="repos">
+                </div>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML
 }
 
 form.addEventListener('submit', (e) => {
