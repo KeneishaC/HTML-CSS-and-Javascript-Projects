85628cf95e2c590b1069fb920d7630b45ad2d42c
keneishac <keneishacorbett@gmail.com>
2021-04-23 13:30:17 -0700

    fixes bug in scritp js file that was not rendering the user profile info

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -20,7 +20,7 @@ async function getUser(username) {
 async function getRepos(username) {
     try {
         const { data } =  await axios(APIURL + username + '/repos?sort=created')
-
+        
         addReopsToCard(data)
         
     } catch (err) {
