74e87e5c8ad65c13274e5ea1148b270b937674db
keneishac <keneishacorbett@gmail.com>
2021-05-13 14:05:41 -0700

    saves notes to local storage

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,19 +1,28 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+
+//checks to see if there are notes in local storage
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click', () => addNewNote())
 
-function addNewNote( text = '') {
+function addNewNote(text = '') {
     const note = document.createElement('div')
     note.classList.add('note')
 
     note.innerHTML = `
-        <div class="tools">
-            <button class="edit"><i class="fas fa-edit"></i></button>
-            <button class="delete"><i class="fas fa-trash-alt"></i></button>
-        </div>
-        <div class="main ${text ? "" : "hidden"}"></div>
-        <textarea class="${text ? "hidden" : ""}"></textarea>
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
     `
+
     const editBtn = note.querySelector('.edit')
     const deleteBtn = note.querySelector('.delete')
     const main = note.querySelector('.main')
@@ -24,6 +33,8 @@ function addNewNote( text = '') {
 
     deleteBtn.addEventListener('click', () => {
         note.remove()
+
+        updateLS()
     })
 
     editBtn.addEventListener('click', () => {
@@ -32,15 +43,23 @@ function addNewNote( text = '') {
     })
 
     textArea.addEventListener('input', (e) => {
-        const { value } = e.target.value
+        const { value } = e.target
 
         main.innerHTML = marked(value)
+
+        updateLS()
     })
 
-    
-
-
-
-
     document.body.appendChild(note)
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    //saves notes to local storage
+    localStorage.setItem('notes', JSON.stringify(notes))
 }
